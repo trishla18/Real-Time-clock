3a38c579697c52762c532c0abf510ad6cdbbefc1
Trishla vagrecha <113227177+trishla18@users.noreply.github.com>
2024-03-25 14:38:46 +0530

    Delete Components directory

--- Components/ClockHeading.jsx
+++
@@ -1,4 +0,0 @@
-function ClockHeading() {
-  return <h1 class="fw-bold">Bharat Clock</h1>;
-}
-export default ClockHeading;
--- Components/ClockSlogan.jsx
+++
@@ -1,8 +0,0 @@
-function ClockSlogan() {
-  return (
-    <p class="fs-4">
-      This is the clock that shows the time in Bharat at all times.
-    </p>
-  );
-}
-export default ClockSlogan;
